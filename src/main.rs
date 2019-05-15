39b848617d6888e028af4dd7bcd0e167aedc6866
Nuxii <nuxii09@gmail.com>
2019-05-15 08:38:41 +0100

    Added parser module.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod input;
+mod parser;
 
 fn main() {
     let expression : String = input::read_buffer();
-    println!("{}", &expression);
+    let tokens : Vec<String> = parser::collect_tokens(&expression, &["->","+","-","*","/"]);
+    for token in tokens {
+        println!("{}", &token);
+    }
 }
