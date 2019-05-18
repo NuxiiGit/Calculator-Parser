92e30bb7465012f6e865e6c7c1d00b1c16b670bf
Nuxii <nuxii09@gmail.com>
2019-05-18 02:19:41 +0100

    Refactored code.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
 mod parser;
 mod input;
 
+use parser::*;
+
 fn main() {
     let expression : String = input::read_buffer();
-    /*let mut operators : Vec<parser::Operator<f64>> = Vec::new();
-    // define operators
-    operators.push(parser::Operator::new("*", 1, |a, b| a * b));
-    operators.push(parser::Operator::new("/", 1, |a, b| a / b));
-    operators.push(parser::Operator::new("+", 0, |a, b| a + b));
-    operators.push(parser::Operator::new("-", 0, |a, b| a - b));
-    */
+    let mut parser : Parser<f64> = Parser::new();
+    // add operators of parser
+    parser.add_op(Operator::new("(_)", 3, |args| args[0]));
+    parser.add_op(Operator::new("|_|", 2, |args| {
+        if args[0] >= 0.0 {
+            args[0]
+        } else {
+            -args[0]
+        }
+    }));
+    parser.add_op(Operator::new("_*_", 1, |args| args[0] * args[1]));
+    parser.add_op(Operator::new("_/_", 1, |args| args[0] / args[1]));
+    parser.add_op(Operator::new("_+_", 0, |args| args[0] + args[1]));
+    parser.add_op(Operator::new("_-_", 0, |args| args[0] - args[1]));
+    // tests
+    print!("Your symbols are:");
+    for symbol in parser.symbols() {
+        print!(" {},", symbol);
+    }
+    println!();
+    match parser.parse(&expression) {
+        Ok(tokens) => {
+            for token in tokens {
+                match token {
+                    Token::Symbol(symbol) => {
+                        println!("Symbol: {}", symbol);
+                    },
+                    Token::Value(value) => {
+                        println!("Value: {}", value);
+                    }
+                }
+            }
+        },
+        Err(msg) => {
+            println!("Error: {}", msg);
+        }
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -14,8 +14,14 @@ impl<T> Parser<T> {
 
     /// Consumes an operator and adds it to the parser's operator vector.
     /// The vector is sorted in order of preceedence.
+    /// Panics when the input operator contains a duplicate symbol.
     #[allow(dead_code)]
-    pub fn add<'a>(&mut self, operator : Operator<T>) {
+    pub fn add_op<'a>(&mut self, operator : Operator<T>) {
+        for symbol in operator.pattern() {
+            if self.symbols().contains(symbol) {
+                panic!("Cannot have ambiguous operator patterns.");
+            }
+        }
         self.operators.push(operator);
         self.operators.sort_by(|a, b| {
             use std::cmp::Ordering;
@@ -33,10 +39,86 @@ impl<T> Parser<T> {
 
     /// Splits a `&str` expression into a `Vec<Token>` of tokens.
     #[allow(dead_code)]
-    pub fn parse(&self, expression : &str) -> Vec<Token<T>> {
+    pub fn parse<'a>(&self, expression : &str) -> Result<Vec<Token<T>>, &'a str> where
+            T : std::str::FromStr {
+        let expression : &str = &expression
+                .replace("\n", "")
+                .replace("\r", "")
+                .replace("\t", "")
+                .replace(" ", "");
         let mut tokens : Vec<Token<T>> = Vec::new();
+        let mut regions : Vec<(usize, String)> = Vec::new();
+        // compile symbol regions
+        for symbol in self.symbols() {
+            'outer:
+            for (i, _) in expression.match_indices(&symbol) {
+                for (j, other) in &regions {
+                    let j : usize = j.to_owned();
+                    if i >= j
+                    && i < j + other.len() {
+                        continue 'outer; // ignore overlap
+                    }
+                }
+                regions.push((i, symbol.clone()));
+            }
+        }
+        regions.sort_by(|(a, _), (b, _)| {
+            use std::cmp::Ordering;
+            if a < b {
+                Ordering::Less
+            } else if a > b {
+                Ordering::Greater
+            } else {
+                Ordering::Equal
+            }
+        });
+        // parse values and add them to the token vector
+        let mut base : usize = 0;
+        for (i, symbol) in regions {
+            match self.parse_value(expression, base, i) {
+                Ok(Some(value)) => {
+                    tokens.push(Token::Value(value));
+                },
+                Ok(None) => {},
+                Err(msg) => {
+                    return Err(msg);
+                }
+            }
+            base = i + symbol.len();
+            tokens.push(Token::Symbol(symbol));
+        }
+        match self.parse_value(expression, base, expression.len()) {
+            Ok(Some(value)) => {
+                tokens.push(Token::Value(value));
+            },
+            Ok(None) => {},
+            Err(msg) => {
+                return Err(msg);
+            }
+        }
+        Ok(tokens)
+    }
 
-        tokens
+    /// Constructs and returns an `Result<ParseTree<T>, &str>` of the input `&[Token<T>]` array.
+    #[allow(dead_code)]
+    pub fn build_tree<'a>(&self, tokens : &[Token<T>]) -> Result<ParseTree<T>, &'a str> where
+            T : std::clone::Clone {
+        if tokens.len() == 0 {
+            return Err("Empty token array.");
+        }
+        // re-format tokens into a vector of tokens containing either parse trees or symbols.
+        let mut trees : Vec<Token<ParseTree<T>>> = Vec::new();
+        for token in tokens {
+            match token {
+                Token::Value(value) => {
+                    trees.push(Token::Value(ParseTree::Leaf(value.to_owned())));
+                },
+                Token::Symbol(symbol) => {
+                    trees.push(Token::Symbol(symbol.to_owned()));
+                }
+            }
+        }
+        Err("Not implemented.")
     }
 
     /// Returns a `Vec<String>` of all possible operator symbols, organised from shortest to longest with no duplicates.
@@ -45,7 +127,8 @@ impl<T> Parser<T> {
         let mut symbols : Vec<String> = Vec::new();
         for operator in &self.operators {
             for symbol in operator.pattern() {
-                if symbol != "_" { // reserved argument symbol
+                if symbol != "_" // reserved argument symbol
+                && !symbols.contains(symbol) {
                     symbols.push(symbol.to_owned());
                 }
             }
@@ -63,6 +146,32 @@ impl<T> Parser<T> {
         });
         symbols
     }
+
+    /// Parses a single value of an expression between a `start` and `end` index.
+    /// Returns a `Result<Option<T>, &str>`. `Ok(None)` is returned when the substring results in an empty string.
+    #[allow(dead_code)]
+    pub fn parse_value<'a>(&self, expression : &str, start : usize, end : usize) -> Result<Option<T>, &'a str> where
+            T : std::str::FromStr {
+        let substring : String = expression
+                .chars()
+                .skip(start)
+                .take(end - start)
+                .collect();
+        if start <= end
+        && &substring != "" {
+            match substring.parse::<T>() {
+                Ok(value) => {
+                    Ok(Some(value))
+                },
+                Err(_) => {
+                    println!("{}, {}", start, end);
+                    Err("Unable to parse expression value.")
+                }
+            }
+        } else {
+            Ok(None)
+        }
+    }
 }
 
 /// An enum used to collect values and operators into a single parent.
@@ -72,11 +181,11 @@ pub enum Token<T> {
     Symbol(String)
 }
 
-/// A recursive data type which is used to represent a token tree.
+/// A recursive data type which is used to represent a parse tree.
 #[allow(dead_code)]
-pub enum TokenTree<'a, T> {
+pub enum ParseTree<'a, T> {
     Leaf(T),
-    Node(Operator<T>, &'a [TokenTree<'a, T>])
+    Node(Operator<T>, &'a [ParseTree<'a, T>])
 }
 
 /// A structure used to define generic operators.
