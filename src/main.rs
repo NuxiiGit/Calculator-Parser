54cc91c3c45ea794b8fbb65cd51607b5e0d1d3dc
Nuxii <nuxii09@gmail.com>
2019-05-22 04:27:15 +0100

    Added exponent operator for testing purposes.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ fn main() {
     let mut parser : Parser<f64> = Parser::new();
     // add operators of parser
     parser.add_op(Operator::new("(_)", 3, |args| args[0]));
-    parser.add_op(Operator::new("|_|", 2, |args| {
+    parser.add_op(Operator::new("|_|", 3, |args| {
         if args[0] >= 0.0 {
             args[0]
         } else {
             -args[0]
         }
     }));
+    parser.add_op(Operator::new("_^_", 2, |args| args[0].powf(args[1])));
     parser.add_op(Operator::new("_*_", 1, |args| args[0] * args[1]));
     parser.add_op(Operator::new("_/_", 1, |args| args[0] / args[1]));
     parser.add_op(Operator::new("_+_", 0, |args| args[0] + args[1]));
