90e17192d07ee4011c0cfdf7a11748c8db32d892
Nuxii <nuxii09@gmail.com>
2019-06-09 21:25:06 +0100

    Tidied code a little.

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,41 @@ mod input;
 use parser::*;
 
 fn main() {
-    let args : Vec<String> = input::read_args(Some(1), None);
-    if args.len() != 2 {
-        println!("Must supply two arguments of the form: -a \"expression\", where a is for arithmetic, and expression is an expression of these two forms.");
-    } else {
-        if &args[0] == "-a" {
-            // arithmetic
-            let mut parser : Parser<f64> = Parser::new();
-            // add operators of parser
-            parser.add_op(Operator::new("(_)", 3, |args| args[0]));
-            parser.add_op(Operator::new("|_|", 3, |args| {
-                if args[0] >= 0.0 {
-                    args[0]
-                } else {
-                    -args[0]
-                }
-            }));
-            parser.add_op(Operator::new("_^_", 2, |args| args[0].powf(args[1])));
-            parser.add_op(Operator::new("_*_", 1, |args| args[0] * args[1]));
-            parser.add_op(Operator::new("_/_", 1, |args| args[0] / args[1]));
-            parser.add_op(Operator::new("_+_", 0, |args| args[0] + args[1]));
-            parser.add_op(Operator::new("_-_", 0, |args| args[0] - args[1]));
-            // parse
-            if let Some(tokens) = parser.parse(&args[1]) {
-                if tokens.len() == 0 {
-                    println!("Error: Empty expression.");
-                } else {
-                    if let Some(tree) = parser.build_token_tree(&tokens) {
-                        if let Some(value) = tree.evaluate() {
-                            println!("Value: {}", value);
-                        } else {
-                            println!("Error: Unable to evaluate parse tree.");
-                        }
-                    } else {
-                        println!("Error: Unable to build parse tree.");
-                    }
-                }
+    // initialise parser
+    let mut parser : Parser<f64> = Parser::new();
+    parser.add_op(Operator::new("(_)", 3, |args| args[0]));
+    parser.add_op(Operator::new("|_|", 3, |args| if args[0] >= 0.0 {args[0]} else {-args[0]}));
+    parser.add_op(Operator::new("_^_", 2, |args| args[0].powf(args[1])));
+    parser.add_op(Operator::new("_*_", 1, |args| args[0] * args[1]));
+    parser.add_op(Operator::new("_/_", 1, |args| args[0] / args[1]));
+    parser.add_op(Operator::new("_+_", 0, |args| args[0] + args[1]));
+    parser.add_op(Operator::new("_-_", 0, |args| args[0] - args[1]));
+    // parse
+    let mut expression = input::read_args_single(Some(1), None);
+    loop {
+        if let Some(tokens) = parser.parse(&expression) {
+            if tokens.len() == 0 {
+                println!("Please enter an expression: ");
+                expression = input::read_buffer();
+                continue; // messy "goto" start
             } else {
-                println!("Error: Unable to parse expression.");
+                if let Some(tree) = parser.build_token_tree(&tokens) {
+                    if let Some(value) = tree.evaluate() {
+                        println!("Value {}", value);
+                    } else {
+                        error("Unable to evaluate parse tree.");
+                    }
+                } else {
+                    error("Unable to build parse tree.");
+                }
             }
         } else {
-            println!("Unsupported expression type.");
+            error("Unable to parse expression.");
         }
+        break;
     }
+}
+
+fn error(msg : &str) {
+    println!("Error: {}", msg);
 }
